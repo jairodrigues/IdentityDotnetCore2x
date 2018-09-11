5a393db8995c61b0b85bca326bdcdba97733c303
Jairo <jairo.goncalves90@gmail.com>
2018-09-10 23:42:14 -0300

    Adc AspNetCoreIdentity a um projeto existente

--- identity.training/Controllers/HomeController.cs
+++ identity.training/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using identity.training.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace identity.training.Controllers
 {
@@ -15,6 +16,7 @@ namespace identity.training.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
---
+++ identity.training/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identity.training.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAdress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter no mínimo {2} e no maximo {1} caracters", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "As senhas devem ser iguais")]
+        public string ConfirmPassword { get; set; }
+    }
+}
